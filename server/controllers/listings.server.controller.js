df66df0f71faa20dfa69ce3ffc90a1f141fcbff2
johnaloomis <johnaloomis@ufl.edu>
2017-09-19 00:15:00 -0400

    Final
    All Tests Passed

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -45,7 +45,31 @@ exports.read = function(req, res) {
 
 /* Update a listing */
 exports.update = function(req, res) {
-  var listing = req.listing;
+	var listing = req.listing;
+	
+	listing.name = req.body.name;
+	listing.code = req.body.code;
+	listing.address = req.body.address;
+  
+  if(req.results)
+  {
+	  listing.coordinates = {
+		  latitude: req.results.lat,
+		  longitude: req.results.lng
+		  }
+  }
+  listing.save(function(err) {
+	  if(err)
+	  {
+		  console.log(err);
+		  res.status(400).send(err);
+	  }
+	  else
+	  {
+		  res.json(listing);
+	  }
+  });
+  
 
   /* Replace the article's properties with the new properties found in req.body */
   /* save the coordinates (located in req.results if there is an address property) */
@@ -55,13 +79,32 @@ exports.update = function(req, res) {
 /* Delete a listing */
 exports.delete = function(req, res) {
   var listing = req.listing;
-
+  listing.remove(function(err) {
+	  if(err)
+	  {
+		  res.status(400).send(err);
+	  }
+	  else{
+		  res.status(200).send();
+	  }
+  });
   /* Remove the article */
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Your code here */
+  Listing.find().sort('code').exec(function(err,listings){
+	 if(err)
+	 {
+		 console.log(err);
+		 res.status(400).send(err);
+	 }		 
+	 else
+	 {
+		 res.json(listings);
+	 }
+  });
 };
 
 /* 
